b05eed8ead116da66c83444f9eca4717827a26ca
Felipe de Oliveira - Prodam <p017020@rede.sp>
2019-10-08 11:09:00 -0300

    Adjust member model

--- allfit-customers-api/Models/Member.cs
+++ allfit-customers-api/Models/Member.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace allfit_customers_api.Models
+﻿namespace allfit_customers_api.Models
 {
     public class Member
     {
         public int id { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
+        public string WhatsApp { get; set; }
+        public byte[] Photo { get; set; }
+        public bool IsActive { get; set; }
     }
 }
